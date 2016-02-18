e5f47aa533273dca3dd400934aa0bfee9caeb975
Jason Stiltner <jasonstiltner@gmail.com>
2016-02-18 15:35:13 -0600

    refactored to es6 syntax

--- controllers/note.js
+++ controllers/note.js
@@ -2,32 +2,49 @@
 
 const Note = require('../models/note');
 
-module.exports.newNote = (req, res) => {
-  res.render('new-note');
-};
+module.exports = {
+  edit (req, res) {
+    res.render('new-note', {note: req.note});
+  },
 
-module.exports.show = (req, res) => {
-  Note.findById(req.params.id, (err, note) => {
-    if (err) throw err;
+  update (req, res) {
+    req.note.update(req.body, (err) => {
+      if (err) throw err;
+      console.log(req.note)
+      res.redirect(`/notes/${req.note._id}`);
+    });
+  },
 
-    res.render('show-note', {note: note});
-  });
-};
+  index (req, res) {
+    Note.find({}, (err, notes) => {
+      if (err) throw err;
 
-module.exports.create = (req, res) => {
-  Note.create(req.body, (err, note) => {
-    if (err) throw err;
+      res.render('notes-index', {notes: notes});
+    });
+  },
 
-    res.redirect(`/notes/${note._id}`);
-  });
-};
+  newNote (req, res) {
+    res.render('new-note');
+  },
 
+  show (req, res) {
+    res.render('show-note', {note: req.note});
+  },
 
-module.exports.destroy = (req, res) => {
-  Note.findByIdAndRemove(req.params._id, (err) => {
-    if (err) throw err;
-  res.send('destroyed!');
+  create (req, res) {
+    Note.create(req.body, (err, note) => {
+      if (err) throw err;
 
-  });
+      res.redirect(`/notes/${note._id}`);
+    });
+  },
+
+  destroy (req, res) {
+    req.note.remove((err) => {
+      if (err) throw err;
+
+      res.redirect('/notes');
+    });
+  }
 };
 
--- routes/note.js
+++ routes/note.js
@@ -1,10 +1,23 @@
 const express = require('express');
 const router = express.Router();
 
+const Note = require('../models/note');
 const note = require('../controllers/note');
 
+router.param('id', (req, res, next, id) => {
+  Note.findById(id, (err, note) => {
+    if (err) throw err;
+
+    req.note = note;
+    next();
+  });
+});
+
+router.get('/notes', note.index);
 router.get('/notes/new', note.newNote);
 router.get('/notes/:id', note.show);
+router.get('/notes/:id/edit', note.edit);
+router.put('/notes/:id', note.update);
 router.delete('/notes/:id', note.destroy);
 router.post('/notes', note.create);
 
