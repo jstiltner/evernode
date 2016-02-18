de6c5d7f3b02ae2b30a189cc7d22f26a270652ab
Jason Stiltner <jasonstiltner@gmail.com>
2016-02-18 10:53:39 -0600

    note saves to database

--- server.js
+++ server.js
@@ -5,9 +5,15 @@
 'use strict';
 const bodyParser = require('body-parser');
 const express = require('express');
+var mongoose = require('mongoose');
 
 const app = express();
 const port = process.env.PORT || 3000;
+//creates a model for MongoDB
+const Note = mongoose.model('Notes', mongoose.Schema({
+  title: String,
+  text: String
+}));
 
 app.set('view engine', 'jade');
 
@@ -24,12 +30,19 @@ app.get('/notes/new', (req, res) => {
 });
 
 app.post('/notes', (req, res) => {
-  console.log(req.body);
+  Note.create(req.body, (err, note) => {
+  if (err) throw err;
+  console.log(note);
   res.redirect('/');
+  });
 });
 
-app.listen(port, () => {
-  console.log(`Evernode server running on port: ${port}`);
-});
 
+mongoose.connect('mongodb://localhost:27017/evernode', (err) => {
+  if (err) throw err;
+
+  app.listen(port, () => {
+    console.log(`Evernode server running on port: ${port}`);
+  });
+});
 
