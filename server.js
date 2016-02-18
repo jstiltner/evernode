9be2563706eea24b7f346a1cea8d7f31031d3f48
Jason Stiltner <jasonstiltner@gmail.com>
2016-02-18 13:39:19 -0600

    delete functionality working

--- server.js
+++ server.js
@@ -3,14 +3,11 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 const mongoose = require('mongoose');
+const methodOverride = require('method-override');
+const note = require('./routes/note');
 
 const app = express();
 const port = process.env.PORT || 3000;
-
-const Note = mongoose.model('Notes', mongoose.Schema({
-  title: String,
-  text: String
-}));
 
 app.set('view engine', 'jade');
 
@@ -18,29 +15,13 @@ app.use(bodyParser.urlencoded({
   extended: false
 }));
 
+app.use(methodOverride('_method'));
+
 app.get('/', (req, res) => {
   res.send('Server Running');
 });
 
-app.get('/notes/new', (req, res) => {
-  res.render('new-note');
-});
-
-app.get('/notes/:id', (req, res) => {
-  Note.findById(req.params.id, (err, note) => {
-    if (err) throw err;
-
-    res.render('show-note', {note: note});
-  });
-});
-
-app.post('/notes', (req, res) => {
-  Note.create(req.body, (err, note) => {
-    if (err) throw err;
-
-    res.redirect(`/notes/${note._id}`);
-  });
-});
+app.use(note);
 
 mongoose.connect('mongodb://localhost:27017/evernode', (err) => {
   if (err) throw err;
