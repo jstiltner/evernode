84a620f83d4ef48146847f44003d43b89d472280
Jason Stiltner <jasonstiltner@gmail.com>
2016-02-18 11:20:06 -0600

    note show page

--- server.js
+++ server.js
@@ -1,15 +1,12 @@
-//express
-//mongoose
-//body-parser
-
 'use strict';
+
 const bodyParser = require('body-parser');
 const express = require('express');
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
 const app = express();
 const port = process.env.PORT || 3000;
-//creates a model for MongoDB
+
 const Note = mongoose.model('Notes', mongoose.Schema({
   title: String,
   text: String
@@ -29,14 +26,21 @@ app.get('/notes/new', (req, res) => {
   res.render('new-note');
 });
 
-app.post('/notes', (req, res) => {
-  Note.create(req.body, (err, note) => {
-  if (err) throw err;
-  console.log(note);
-  res.redirect('/');
+app.get('/notes/:id', (req, res) => {
+  Note.findById(req.params.id, (err, note) => {
+    if (err) throw err;
+
+    res.render('show-note', {note: note});
   });
 });
 
+app.post('/notes', (req, res) => {
+  Note.create(req.body, (err, note) => {
+    if (err) throw err;
+
+    res.redirect(`/notes/${note._id}`);
+  });
+});
 
 mongoose.connect('mongodb://localhost:27017/evernode', (err) => {
   if (err) throw err;
