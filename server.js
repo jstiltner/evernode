73a3f871f94aa7dd90a831afd8ee56ab8ced37b3
Jason Stiltner <jasonstiltner@gmail.com>
2016-02-18 10:37:13 -0600

    create new note feature completed

--- server.js
+++ server.js
@@ -3,16 +3,31 @@
 //body-parser
 
 'use strict';
-
+const bodyParser = require('body-parser');
 const express = require('express');
-
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.set('view engine', 'jade');
+
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
 app.get('/', (req, res) => {
   res.send('Server Running');
 });
+
+app.get('/notes/new', (req, res) => {
+  res.render('new-note');
+});
+
+app.post('/notes', (req, res) => {
+  console.log(req.body);
+  res.redirect('/');
+});
+
 app.listen(port, () => {
   console.log(`Evernode server running on port: ${port}`);
 });
